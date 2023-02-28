fbabc87ab0048d2c78cfb640b7053a9d40a837f7
Fadwahigga <fadwa.ali20@gmail.com>
2023-02-28 23:41:57 +0200

    new methods

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ new Vue ({
     data:{
     firstname: 'Fadwa',
     lastname:'Ali',
-    age:24
+    age:24,
+    salary:3000,
     },
     methods:{
         myName(){
             return "My Name Is Fadwa"
+        },
+        mySalary(){
+            return this.salary
+        },
+        putYourName(name){
+            return name
         }
     }
 })
