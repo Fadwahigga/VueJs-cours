4b969f1cf5606dec0c0c3ad4d3aa5ad72ae75728
Fadwahigga <fadwa.ali20@gmail.com>
2023-03-02 11:43:58 +0200

    Methods

--- vue-project/src/components/FirstCom.vue
+++ vue-project/src/components/FirstCom.vue
@@ -1,8 +1,14 @@
 <template>
-<h1>New Component</h1>
+<div><h1>New Component {{ "fadwaaaaaaaaaaaa".length }}</h1>
+<h1>Name {{ getName("fadwa ali") }}</h1></div>
 </template>
 <script>
 export default {
-    name : "Newwww"
+    name : "Newwww",
+    methods: {
+        getName(name){
+            return name
+        }
+    }
 }
 </script>
