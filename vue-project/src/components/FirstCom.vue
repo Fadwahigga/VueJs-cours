a2cff8b9458f722b0c24f172e09fae5aaa217f2f
Fadwahigga <fadwa.ali20@gmail.com>
2023-03-03 17:53:34 +0200

    count event

--- vue-project/src/components/FirstCom.vue
+++ vue-project/src/components/FirstCom.vue
@@ -1,18 +1,29 @@
 <template>
-<div><h1>New Component {{ "fadwaaaaaaaaaaaa".length }}</h1>
-<h1>Name {{ getName("fadwa ali") }}</h1><button v-on:click="eventFun()">Click</button></div>
-
+  <div>
+    <h1>New Component {{ "fadwaaaaaaaaaaaa".length }}</h1>
+    <h1>Name {{ getName("fadwa ali") }}</h1>
+    <button v-on:click="eventFun()">Click</button> 
+     <button v-on:click="icreCount()">count</button>
+     <h1>{{ count }}</h1>
+  </div>
 </template>
 <script>
 export default {
-    name : "Newwww",
-    methods: {
-        getName(name){
-            return name
-        },
-        eventFun(){
-            alert("Event Called")
-        }
+  data(){
+     return {count:0}
+  },
+
+  methods: {
+    getName(name) {
+      return name;
+    },
+    eventFun() {
+      alert("Event Called");
+    },
+    icreCount(){
+        this.count=this.count+1
+
     }
-}
+  },
+};
 </script>
