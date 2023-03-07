676a3d526456d845804f85d032026d48dc45627c
Fadwahigga <fadwa.ali20@gmail.com>
2023-03-07 14:40:02 +0200

    create inputs

--- vue-project/src/components/FormR.vue
+++ vue-project/src/components/FormR.vue
@@ -1,4 +1,15 @@
 <template>
+  <div>
     <h1>Form component</h1>
+    <form>
+      <label>Name :</label> <input type="text" placeholder="Enter Email" />
+      <br /><br />
+
+      <label>Password : </label>
+      <input type="password" placeholder="Enter Password" /> <br /><br />
+      <br /><br />
+      <button type="button">LogIn</button>
+    </form>
+  </div>
 </template>
 <script></script>
