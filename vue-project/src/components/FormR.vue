3767f19f7cea53ad31da39fb5aab1eff137b4055
Fadwahigga <fadwa.ali20@gmail.com>
2023-03-08 11:34:12 +0200

    form

--- vue-project/src/components/FormR.vue
+++ vue-project/src/components/FormR.vue
@@ -15,22 +15,18 @@
         v-model="form.password"
       />
       <br /><br />
+      <h1>Country : </h1>
       <select v-model="form.country">
         <option>Sudan</option>
         <option>USA</option>
       </select>
       <br /><br />
-      <label>Name : </label>
-      <input type="text" placeholder="Enter Email" v-model="form.email" />
-
+      <h1>Gender :</h1>
+      <label>Female: </label>
+      <input type="radio" name="gender" v-model="form.gender" />
       <br /><br />
-
-      <label>Password : </label>
-      <input
-        type="password"
-        placeholder="Enter Password"
-        v-model="form.password"
-      />
+      <label>Male : </label>
+      <input type="radio" name="gender" v-model="form.gender" />
       <br /><br />
       <button type="button" v-on:click="Login">LogIn</button>
     </form>
